a6bd9ccbac68fccc89d0a6dabe60fc30db7d22a6
oliullah05 <mdolihasan@gmail.com>
2024-04-28 19:15:54 +0600

    navbar done

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Photo Feed",
+  description: "A photo feed app build with nextjs",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+
+      <body className={inter.className}>
+      <Navbar></Navbar>
+       <div className="container my-4 lg:my-8">
+       {children}
+       </div>
+
+  
+        </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      
+      <h1>Photo Feed</h1>
+    </div>
+
+  );
+}
